2d4205f11c6199d5e74a44daba9ed71a4223a0f2
Anas Dinari <anasdinari@gmail.com>
2024-07-21 08:14:25 +0200

    setup and configured database, created question and choice models

--- myPollApp/myPollApp/settings.py
+++ myPollApp/myPollApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'polls.apps.PollsConfig',#The PollsConfig class is in the polls/apps.py file
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'myPollApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypolldb',
+        'USER': 'polluser',
+        'PASSWORD': 'polldbpass',
+        'HOST': 'localhost',
+        'PORT': '',  # Leave empty for default
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
