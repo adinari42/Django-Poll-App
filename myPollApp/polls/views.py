d251105a9bc24c8440c9664da93087769d9a54ca
Anas Dinari <anasdinari@gmail.com>
2024-07-24 03:20:29 +0200

    added form and vote inputs with post request actions in detail template

--- myPollApp/polls/views.py
+++ myPollApp/polls/views.py
@@ -1,7 +1,10 @@
+from django.db.models import F
 from django.http import Http404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+
+from .models import Choice, Question
 
 #Each view is responsible for doing one of two things: returning an HttpResponse object
 # containing the content for the requested page, or raising an exception such as Http404
@@ -16,13 +19,31 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])#request.POST['choice'] returns the ID of the selected choice
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1#instructs the database to increase the vote count by 1.
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))#HttpResponseRedirect redirects the user to the url argument
+        #reverse() helps construct the url easier using name of the view and view url variable
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
